74483ba00e0ed7748729e9a147de085ac6181da2
mcrews <matthewcrews@gmail.com>
2018-02-08 15:26:35 -0800

    Begin refactor of new match of Commands

--- EventSourcingExample/Scratch.fsx
+++ EventSourcingExample/Scratch.fsx
@@ -4,50 +4,86 @@
 
 open System
 
-type EventSource = string -> array<Events.Event> option
-type OrderSource = string -> Order option
-type EventAdder = string -> Events.Event -> unit
-
-type Created = {
-    Id : Guid
-}
-
-type LineAdded = {
-    Id : Guid
+type OrderLine = {
     LineId : int
     ItemId : string
     Amount : decimal
 }
 
-type LineRemoved = {
+type Order = {
     Id : Guid
-    LineId : int
+    Lines : OrderLine list
 }
+
+let init = {
+    Id = Guid.Parse "00000000-0000-0000-0000-000000000000"
+    Lines = []
+}
+
+
+module Events =
+    type Created = {
+        Id : Guid
+    }
+
+    type LineAdded = {
+        Id : Guid
+        LineId : int
+        ItemId : string
+        Amount : decimal
+    }
+
+    type LineRemoved = {
+        Id : Guid
+        LineId : int
+    }
     
-type Event =
-| Created of Created
-| LineAdded of LineAdded
-| LineRemoved of LineRemoved
-with 
-    member x.Id = 
-        match x with 
-        | Created { Id = id } -> id
-        | LineAdded { Id = id } -> id
-        | LineRemoved { Id = id } -> id
+    type Event =
+    | Created of Created
+    | LineAdded of LineAdded
+    | LineRemoved of LineRemoved
+    with 
+        member x.Id = 
+            match x with 
+            | Created { Id = id } -> id
+            | LineAdded { Id = id } -> id
+            | LineRemoved { Id = id } -> id
+
+type EventSource = string -> array<Events.Event> option
+type OrderSource = string -> Order option
+type EventAdder = string -> Events.Event -> unit
+
+type Create = Events.Created
+type AddLine = Events.LineAdded
+type RemoveLine = Events.LineRemoved
+
+open Events
 
 let ensureOrder (orderSource: OrderSource) (event: Events.Event) = 
-    match orderSource (string event.Id) with
-    | None -> Result.Error "Order already exists"
+    match (orderSource (string event.Id)) with
     | Some order -> Result.Ok order
+    | None -> Result.Error "Order already exists"
 
 
-let addLine (eventAdder : EventAdder) (({ Id = id }: AddLine) as c) order =
-    match not(order.Lines |> List.exists (fun l -> l.LineId = c.LineId)) with
+let addLine (eventAdder : EventAdder) (newLine : AddLine) order =
+    match not(order.Lines |> List.exists (fun l -> l.LineId = newLine.LineId)) with
     | true ->
-        eventAdder (string id) (Event.LineAdded c)
+        eventAdder (string newLine.Id) (Event.LineAdded newLine)
         Result.Ok "Line Added"
     | false ->
         Result.Error "Line already exists"
+
+let create (eventAdder : EventAdder) (createOrder : Create) =
+    eventAdder (string createOrder.Id) (Event.Created createOrder)
+    Result.Ok "Order created"
+
+let removeLine (eventAdder : EventAdder) (remLine : RemoveLine) order =
+    match (order.Lines |> List.exists (fun l -> l.LineId = remLine.LineId)) with
+    | true ->
+        eventAdder (string remLine.Id) (Event.LineRemoved remLine)
+        Result.Ok "Line removed"
+    | false ->
+        Result.Error "Line does not exist"
 
 // so now all your domain handling code validates the order finding first with 'ensureOrder', then passes that order into the event given
 
