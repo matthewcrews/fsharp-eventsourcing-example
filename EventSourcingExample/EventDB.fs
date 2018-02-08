cd7413da40be227a21a1117f51cac289249196c2
mcrews <matthewcrews@gmail.com>
2018-02-08 11:21:42 -0800

    Initial commit

---
+++ EventSourcingExample/EventDB.fs
@@ -0,0 +1,38 @@
+﻿module EventDB
+
+open System.IO
+
+
+
+module Json = 
+    open Newtonsoft.Json 
+    let serialize obj = 
+        JsonConvert.SerializeObject(obj, Formatting.Indented)
+    let deserialize<'a> str = 
+        JsonConvert.DeserializeObject<'a> str
+
+let read<'a> dir topic : 'a array =
+    let file = Path.Combine(dir, topic + ".json")
+    let text = File.ReadAllText(file)
+    Json.deserialize<'a array>(text)
+
+let readAfter<'a> dir topic (i : int) =
+    (read<'a> dir topic).[i..]
+        
+let write<'a> dir topic e =
+    let writeEvents file events =
+        let data = Json.serialize events
+        File.WriteAllText(file, data)
+
+    if not(Directory.Exists(dir)) then
+        Directory.CreateDirectory(dir) |> ignore
+
+    let file = Path.Combine(dir, topic + ".json")
+
+    if not(File.Exists(file)) then
+        [|e|]
+        |> writeEvents file
+    else
+        let events = read<'a> dir topic
+        Array.append events [|e|]
+        |> writeEvents file
---
+++ EventSourcingExample/OrderDomain.fs
@@ -0,0 +1,114 @@
+﻿module OrderDomain
+
+open System
+
+type OrderLine = {
+    LineId : int
+    ItemId : string
+    Amount : decimal
+}
+
+type Order = {
+    Id : Guid
+    Lines : OrderLine list
+}
+
+let init = {
+    Id = Guid.Parse "00000000-0000-0000-0000-000000000000"
+    Lines = []
+}
+
+module Events =
+    type Created = {
+        Id : Guid
+    }
+
+    type LineAdded = {
+        Id : Guid
+        LineId : int
+        ItemId : string
+        Amount : decimal
+    }
+
+    type LineRemoved = {
+        Id : Guid
+        LineId : int
+    }
+
+    type Event =
+    | Created of Created
+    | LineAdded of LineAdded
+    | LineRemoved of LineRemoved
+
+    let private create (order : Order) (c : Created) =
+        {order with Id = c.Id}
+
+    let private addLine (order : Order) (a : LineAdded) =
+        let newLines = order.Lines @ [{LineId = a.LineId; ItemId = a.ItemId; Amount = a.Amount}]
+        {order with Lines = newLines}
+
+    let private removeLine (order : Order) (r : LineRemoved) =
+        let newLines = order.Lines |> List.where (fun l -> l.LineId <> r.LineId)
+        {order with Lines = newLines}
+
+    let map (order : Order) (e : Event) =
+        match e with
+        | Created c -> create order c
+        | LineAdded a -> addLine order a
+        | LineRemoved r -> removeLine order r
+
+type EventSource = string -> array<Events.Event> option
+type OrderSource = string -> Order option
+type EventAdder = string -> Events.Event -> unit
+
+let orderSourceComposer (eventSource : EventSource) =
+    fun (id : string) ->
+        eventSource id
+        |> Option.map (Array.fold Events.map init)
+
+
+module Commands =
+    open Events
+
+    type Create = Events.Created
+    type AddLine = Events.LineAdded
+    type RemoveLine = Events.LineRemoved
+
+    let create (orderSource : OrderSource) (eventAdder : EventAdder) (c : Create) =
+        let id = c.Id |> string
+        let order = orderSource id
+        match order with
+        | Some _ ->
+            Result.Error "Order already exists"
+        | None ->
+            let e = Event.Created c
+            eventAdder id e
+            Result.Ok "Order created"
+
+    let addLine (orderSource : OrderSource) (eventAdder : EventAdder) (c : AddLine) =
+        let id = c.Id |> string
+        let order = orderSource id
+        match order with
+        | Some o ->
+            match not(o.Lines |> List.exists (fun l -> l.LineId = c.LineId)) with
+            | true ->
+                eventAdder id (Event.LineAdded c)
+                Result.Ok "Line Added"
+            | false ->
+                Result.Error "Line already exists"
+        | None ->
+            Result.Error "Order does not exist"
+
+    let removeLine (orderSource : OrderSource) (eventAdder : EventAdder) (c : RemoveLine) =
+        let id = c.Id |> string
+        let order = orderSource id
+        match order with
+        | Some o ->
+            match o.Lines |> List.exists (fun l -> l.LineId = c.LineId) with
+            | true ->
+                eventAdder id (Event.LineRemoved c)
+                Result.Ok "Line Added"
+            | false ->
+                Result.Error "Line does not exist"
+        | None ->
+            Result.Error "Order does not exist"
