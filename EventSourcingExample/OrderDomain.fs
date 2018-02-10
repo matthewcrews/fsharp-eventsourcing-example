242ee0a4fc2cc0011630b707c52e985af159954c
mcrews <matthewcrews@gmail.com>
2018-02-09 21:11:31 -0800

    Reformulating error checking

--- EventSourcingExample/OrderDomain.fs
+++ EventSourcingExample/OrderDomain.fs
@@ -95,13 +95,88 @@ module Commands =
             | AddLine { Id = id } -> id
             | RemoveLine { Id = id } -> id
 
+    type CommandResult = Result<string, string>
+    type ErrorMessage = string
 
-    type Create = EventWriter -> Created -> Result<string, string>
+    type OrderExistsTest = Order option -> Result<Order, ErrorMessage>
+    type LineExistsTest = Order -> Result<Order, ErrorMessage>
+    type LineDoesNotExistTest = Order -> Result<Order, ErrorMessage>
+
+    type CheckCreate = Order option -> Created -> Result<Created, ErrorMessage>
+    let composeCheckCreate (check : OrderExistsTest) : CheckCreate =
+        fun (order : Order option) (created : Created) ->
+            match check order with
+            | Result.Ok _ -> Result.Ok created
+            | Result.Error err -> Result.Error err
+
+    type CheckAddLine = Order option -> LineAdded -> Result<LineAdded, ErrorMessage>
+    let composeCheckAddLine (testExist : OrderExistsTest) (testAdd : LineDoesNotExistTest) =
+        fun (order : Order option) (add : LineAdded) ->
+            match testExist order |> Result.bind testAdd with
+            | Result.Ok _ -> Result.Ok add
+            | Result.Error err -> Result.Error err
+
+    type CheckRemoveLine = Order option -> LineRemoved -> Result<LineRemoved, ErrorMessage>
+    let composeCheckRemoveLine (testExist : OrderExistsTest) (testRemove : LineExistsTest) =
+        fun (order : Order option) (remove : LineRemoved) ->
+            match testExist order |> Result.bind testRemove with
+            | Result.Ok _ -> Result.Ok remove
+            | Result.Error err -> Result.Error err
+
+    type CheckCommand = Order option -> Command -> Result<Command, ErrorMessage>
+
+    //// Bad
+    //let testCreate (order : Order option) (create : Created) =
+    //    match order with
+    //    | Some r -> false
+    //    | None -> true
+
+    // Good
+    let testCreate (order : Order option) (create : Created) =
+        if Option.isNone order then 
+            Result.Ok create
+        else
+            Result.Error "Order already exists"
+        
+
+
+    (*
+
+    Okay, here is the problem, I am trying to combine too many checks into a single function.
+    
+    //Example
+    let testAddLine (order : Order option) (add : LineAdded) =
+        let test (a : LineAdded) o = not(o.Lines |> List.exists (fun l -> l.LineId = a.LineId))
+        Option.exists (test add) order
+
+    Here I am trying to check that the order exists and that the OrderLine does not already exist.
+    This is combining two different 'functions' into one. Better to have a function that
+    check for existence and then having a function that checks for the Line not existing.
+    The ExistenceCheck has the signature `Order option -> Result<Order, string>` while 
+    the LineCheck has the signature `Order -> Result<Order, string>`.
+
+    *)
+
+
+
+    let testRemoveLine (order : Order option) (remove : LineRemoved) =
+        let test (r : LineRemoved) o  = o.Lines |> List.exists (fun l -> l.LineId = r.LineId)
+        Option.exists (test remove) order
+
+    let composeTester (testCreate : TestCreate) (testAdd : TestAddLine) (testRemove : TestRemoveLine) : Tester =
+        fun (order : Order option) (cmd : Command) ->
+            match cmd with
+            | Create c -> testCreate order c
+            | AddLine a -> testAdd order a
+            | RemoveLine r -> testRemove order r
+        
+
+    type Create = EventWriter -> Created -> CommandResult
     let create (writer : EventWriter) (c : Created) =
         writer (string c.Id) (Event.Created c)
         Result.Ok "Order Added"
 
-    type AddLine = EventWriter -> Order -> LineAdded -> Result<string, string>
+    type AddLine = EventWriter -> Order -> LineAdded -> CommandResult
     let addLine (writer : EventWriter) (order : Order) (a : LineAdded) =
         if not(order.Lines |> List.exists (fun l -> l.LineId = a.LineId)) then
             writer (string a.Id) (Event.LineAdded a)
@@ -109,7 +184,7 @@ module Commands =
         else
             Result.Error "Line already exists"
 
-    type RemoveLine = EventWriter -> Order -> LineRemoved -> Result<string, string>
+    type RemoveLine = EventWriter -> Order -> LineRemoved -> CommandResult
     let removeLine (writer : EventWriter) (order : Order) (r : LineRemoved) =
         if order.Lines |> List.exists (fun l -> l.LineId = r.LineId) then
             writer (string r.Id) (Event.LineRemoved r)
@@ -117,17 +192,24 @@ module Commands =
         else
             Result.Error "Line does not exist"
 
+    type CommandHandler = Command -> CommandResult
+    type CommandHandlerComposer = Create -> AddLine -> RemoveLine -> OrderSource -> EventWriter -> CommandHandler
 
-    type CommandHandler = Command -> Result<string, string>
-    let composeCommandHandler (create : Create) (add : AddLine) (remove : RemoveLine) (orderSource : OrderSource) (writer : EventWriter) : CommandHandler =
-        fun (cmd : Command) ->
-            let order = orderSource (string cmd.Id)
-            match cmd, order with
-            | Create c, None -> create writer c
-            | Create c, Some o -> Result.Error "Order already exists"
-            | _, None -> Result.Error "Order does not exist"
-            | AddLine l, Some o -> add writer o l
-            | RemoveLine l, Some o -> remove writer o l
+    //type CommandHandlerBuilder = Create -> AddLine -> RemoveLine -> (OrderSource -> EventWriter -> Result<string, string>)
+    //let commandHandlerBuilder (create : Create) (add : AddLine) (remove : RemoveLine) =
+    //    fun (orderSource : OrderSource) (writer : EventWriter) ->
+            
+
+    //type CommandHandler = Command -> Result<string, string>
+    //let composeCommandHandler (create : Create) (add : AddLine) (remove : RemoveLine) (orderSource : OrderSource) (writer : EventWriter) : CommandHandler =
+    //    fun (cmd : Command) ->
+    //        let order = orderSource (string cmd.Id)
+    //        match cmd, order with
+    //        | Create c, None -> create writer c
+    //        | Create c, Some o -> Result.Error "Order already exists"
+    //        | _, None -> Result.Error "Order does not exist"
+    //        | AddLine l, Some o -> add writer o l
+    //        | RemoveLine l, Some o -> remove writer o l
             
 
     
